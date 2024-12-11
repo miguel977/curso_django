ebe33500f2019ce935fc774d8750382ae8c632af
miguel977 <miguel645@gmail.com>
2024-12-11 08:28:49 -0300

    refatorando as funções

--- django/urls_views/desafios/views.py
+++ django/urls_views/desafios/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
+desafios_dia_semana = {
+    'domingo': 'Ler Livro sobre Django',
+    'segunda': 'Assistir Série Breaking Bad',
+    'terça': 'Estudar programação Python',
+    'quarta': 'Fazer um curso de Design Gráfico',
+    'quinta': 'Praticar um Esporte',
+    'sexta': None,
+    'sábado': 'Ir a Igreja'
+}
 
 def desafio_semana_numero(request, dia):
-    return HttpResponse(dia)
+    dias = list(desafios_dia_semana.keys())
+    if dia > len(dias) or dia == 0:
+        return HttpResponseNotFound("Dia inválido")
+    dia_escolhido = dias[dia -1]
+    return HttpResponseRedirect("/desafios/" + dia_escolhido)
     
 
 def desafio_semana(request, dia):
-    desafio = ""
-    if dia == 'domingo':
-        desafio = 'Ler Livro sobre django'
-    elif dia == 'segunda':
-        desafio = 'Assistir Série Breacker bad'
-    elif dia == 'terça':
-        desafio = 'Estudar programação em python'
-    else:
-        return HttpResponseNotFound("Não há desafio para esse dia informado")
-    
+    try:
+        desafio = desafios_dia_semana[dia]
+        # return render(request, "desafios/desafio.html", {
+        #     'desafio':desafio,
+        #     'dia':dia
+        # })
+    except:
+        return HttpResponse("Para esse parâmetro não há desafio")
     return HttpResponse(desafio)
